c42777e9356d38ae577db508625778cc3a941689
baebug <baebug@MacBook-Air.local>
2021-08-16 19:39:25 +0900

    c

---
+++ c/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+main 함수는
+프로그램이 시작되는 함수.
+CPU의 명령어 레지스터에
+프로그램의시작 명령어 주소값을 전달
+int - return 0 (좋은 습관?)
+확실한건 주석은 좋은 습관!
+*/
+int main()
+{
+    printf("Hello World! \n");
+    return 0;
+}
